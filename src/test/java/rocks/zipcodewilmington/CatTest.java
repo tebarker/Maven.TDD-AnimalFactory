ac81de1b9107055e166738e4142c730d89f22045
Tenae <tenae@Zipcoders-MacBook-Pro-107.local>
2022-07-02 14:29:05 -0400

    CatTest Complete

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -2,7 +2,9 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Mammal;
 
 import java.util.Date;
 
@@ -17,6 +19,7 @@ public class CatTest {
     // TODO - Create tests for `Integer getId()`
     // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
     // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+
 
 
     @Test
@@ -38,6 +41,119 @@ public class CatTest {
         Assert.assertEquals(givenName, retrievedName);
         Assert.assertEquals(givenBirthDate, retrievedBirthDate);
         Assert.assertEquals(givenId, retrievedId);
+    }
+
+
+    @Test
+    public void testSetName(){
+        //Given (cat data)
+        String givenName = "Nala";
+        Date givenBirthDate= new Date();
+        Integer givenId = 28;
+
+        // When (a cat is constructed, we set a new name for cat, and we retrieve data from said cat)
+        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+        String newName = "Sarabi";
+        String retrievedName = cat.getName();
+
+        //Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(givenName, retrievedName);
+
+
+    }
+
+    @Test
+    public  void testSetBirthDate() {
+        //Given (cat data)
+        String givenName = "Benni";
+        Date givenBirthDate= new Date();
+        Integer givenId = 28;
+
+        // When
+        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+        cat.setBirthDate(givenBirthDate);
+        Date retrievedBirthDate = cat.getBirthDate();
+
+        // Then
+        Assert.assertEquals(givenBirthDate, retrievedBirthDate);
+    }
+    @Test
+    public void testSpeak() {
+        //Given (cat data)
+        String givenName = "Benni";
+        Date givenBirthDate = new Date();
+        Integer givenId = 28;
+
+        // When
+        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+        String expectedString = "meow!";
+        String actualString = cat.speak();
+
+        // Then
+        Assert.assertEquals(expectedString, actualString);
+    }
+    @Test
+    public void testEat() {
+        //Given (cat data)
+        String givenName = "Benni";
+        Date givenBirthDate = new Date();
+        Integer givenId = 28;
+        Integer expectedNumberOfMealsEaten = 1;
+
+        // When
+        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+        Food food = new Food();
+        cat.eat(food);
+        Integer actualNumberOfMealsEaten = cat.getNumberOfMealsEaten();
+
+
+
+        // Then
+        Assert.assertEquals(expectedNumberOfMealsEaten, actualNumberOfMealsEaten);
+    }
+
+    @Test
+    public void testGetId() {
+        //Given (cat data)
+        String givenName = "Benni";
+        Date givenBirthDate = new Date();
+        Integer givenId = 28;
+        Integer expectedId = 1;
+
+        // When
+        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+        Integer actualId = cat.getId();
+
+        // Then
+        Assert.assertEquals(expectedId, actualId);
+    }
+
+    @Test
+    public void testAnimalInheritance() {
+        //Given (cat data)
+        String givenName = "Benni";
+        Date givenBirthDate = new Date();
+        Integer givenId = 28;
+
+        //When
+        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+
+        // Then
+        Assert.assertTrue(cat instanceof Animal);
+    }
+
+    @Test
+    public void testMammalInheritance() {
+        //Given (cat data)
+        String givenName = "Benni";
+        Date givenBirthDate = new Date();
+        Integer givenId = 28;
+
+        //When
+        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+
+        // Then
+        Assert.assertTrue(cat instanceof Mammal);
     }
 
 }
